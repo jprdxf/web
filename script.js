443acf02ec85fba56e8098bc3e285d5d56e84e6b
jprdxf <bautistagd14@gmail.com>
2025-06-01 17:44:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.btn');
-  btn.addEventListener('click', () => {
-    btn.classList.add('clicked');
-    setTimeout(() => {
-      btn.classList.remove('clicked');
-    }, 300);
-  });
-});
